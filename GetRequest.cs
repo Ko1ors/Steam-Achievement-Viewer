049533d47a9633b2c1e76471b9dd5b5378f437d8
Ko1ors <kolorsnik181200@gmail.com>
2020-07-09 23:11:26 +0300

    Добавьте файлы проекта.

---
+++ GetRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace AchievementTest
+{
+    class GetRequest
+    {
+        public static XmlDocument XmlRequest(string RequestUrl) {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(RequestUrl);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(new StreamReader(response.GetResponseStream(), Encoding.UTF8));
+                return doc;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace AchievementTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //XmlDocument doc = GetRequest.XmlRequest("https://steamcommunity.com/id/Ko1ors/stats/1174180/achievements/?xml=1");
+            //XmlNode xRoot = doc.DocumentElement;
+            //XmlNode GameNode = xRoot.SelectSingleNode("game");
+            
+            //TextBlock testText = new TextBlock();
+            //testText.Text = GameNode.SelectSingleNode("gameName").InnerText;
+            //GamesList.Children.Add(testText);
+            //foreach (XmlNode n in GameNode.ChildNodes)
+            //{
+            //    TextBlock testText = new TextBlock();
+            //    testText.Text = n.Name;
+            //    GamesList.Children.Add(testText);
+            //}
+        }
+    }
+}
