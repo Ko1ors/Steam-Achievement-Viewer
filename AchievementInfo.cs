0e8e552281557726fa582f50f714568942a24c47
N-Husky <N-Husky@users.noreply.github.com>
2020-07-30 19:54:54 +0300

    Исправил текст в страницах

--- AchievementInfo.cs
+++ AchievementInfo.cs
@@ -10,6 +10,7 @@ namespace AchievementTest
     {
         public string ImagePath { get; set; }
         public string GameLogoPath { get; set; }
+        public string GameName { get; set; }
         public string AchievementName {get;set;}
         public string AchievementDesctiption { get; set; }
         public string AchievementDate { get; set; }
--- Pages/LastAchievedPage.xaml.cs
+++ Pages/LastAchievedPage.xaml.cs
@@ -19,3 +19,4 @@ namespace AchievementTest.Pages
         }
     }
 }
+ 
