1d7fb2b96ef20f2a8d177ac2bf584f61412c9da5
Ko1ors <kolorsnik181200@gmail.com>
2022-05-19 18:41:04 +0300

    added indication for selected tab, disabled navigation during sync

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using SteamAchievementViewer.Pages;
+﻿using SteamAchievementViewer.Models;
+using SteamAchievementViewer.Pages;
 using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace SteamAchievementViewer
@@ -10,11 +13,26 @@ namespace SteamAchievementViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindowModel Model { get; set; }
 
         public MainWindow()
         {
-
             InitializeComponent();
+            Model = new MainWindowModel();
+            Model.NavigationPages = new()
+            {
+                new NavigationPageElement{ Type = typeof(MainPageInfo), Title = Properties.Resources.MainPage },
+                new NavigationPageElement{ Type = typeof(AuthPage), Title = Properties.Resources.AuthPage },
+                new NavigationPageElement{ Type = typeof(LastAchievedPage), Title = Properties.Resources.LastAchievedPage },
+                new NavigationPageElement{ Type = typeof(CloseAchievements), Title = Properties.Resources.ClosestPage },
+                new NavigationPageElement{ Type = typeof(CloseAllAchievements), Title = Properties.Resources.ClosestAllPage },
+                new NavigationPageElement{ Type = typeof(RareAchievements), Title = Properties.Resources.RarestPage },
+            };
+            Model.IsNavigationAvailable = true;
+            NavigateTo(Model.NavigationPages.First());
+
+            DataContext = this;
+
             Manager.Start();
             if (Manager.profile != null)
                 UpdateAvatar(Manager.profile.AvatarFull);
@@ -22,48 +40,29 @@ namespace SteamAchievementViewer
 
         public void UpdateAvatar(string IconPath)
         {
-            ((MainWindow)System.Windows.Application.Current.MainWindow).ProfileAvatar.Source = new BitmapImage(new Uri(IconPath));
+            ProfileAvatar.Source = new BitmapImage(new Uri(IconPath));
         }
 
+        private void Navigation_Click(object sender, RoutedEventArgs e)
+        {
+            if (Model.IsNavigationAvailable)
+            {
+                var button = (Button)sender;
+                var navigationPage = button.CommandParameter as NavigationPageElement;
+                NavigateTo(navigationPage);
+            }
+        }
+
+        private void NavigateTo(NavigationPageElement navigationPage)
+        {
+            Model.NavigationPages.ForEach(np => np.Selected = false);
+            navigationPage.Selected = true;
+            Information.Content = Manager.GetPageObject(navigationPage.Type);
+        }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
-        }
-
-        private void AuthorizationButton_clicked(object sender, RoutedEventArgs e)
-        {
-            Information.Content = Manager.GetPageObject<AuthPage>();
-        }
-        private void LastAchievedButton_clicked(object sender, RoutedEventArgs e)
-        {
-            Information.Content = Manager.GetPageObject<LastAchievedPage>();
-        }
-
-        private void CloseAchievementButton_clicked(object sender, RoutedEventArgs e)
-        {
-            Information.Content = Manager.GetPageObject<CloseAchievements>();
-        }
-
-        private void CloseAllAchievementButton_clicked(object sender, RoutedEventArgs e)
-        {
-
-            Information.Content = Manager.GetPageObject<CloseAllAchievements>();
-        }
-
-        private void RareAchievementButton_clicked(object sender, RoutedEventArgs e)
-        {
-            Information.Content = Manager.GetPageObject<RareAchievements>();
-        }
-
-        private void MainInfoButton_cliked(object sender, RoutedEventArgs e)
-        {
-            Information.Content = Manager.GetPageObject<MainPageInfo>();
-        }
-
-        private void SettingsButton_clicked(object sender, RoutedEventArgs e)
-        {
-            Information.Content = Manager.GetPageObject<SettingsPage>();
         }
     }
 }
