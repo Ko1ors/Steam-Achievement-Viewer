43347270a5baead36dd9f45e045629685561861a
Ko1ors <kolorsnik181200@gmail.com>
2023-02-05 21:49:53 +0200

    queue service and repository added

---
+++ Sav.Common/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Sav.Common.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+
+        IEnumerable<T> GetAll();
+
+        T? Find(Predicate<T> match);
+    }
+}
---
+++ Sav.Common/Repositories/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Sav.Common.Interfaces;
+
+namespace Sav.Common.Repositories
+{
+    public class Repository<T> : IRepository<T>
+    {
+        private readonly List<T> _items;
+
+        public Repository()
+        {
+            _items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _items;
+        }
+
+        public T? Find(Predicate<T> match)
+        {
+            return _items.Find(match);
+        }
+    }
+}
