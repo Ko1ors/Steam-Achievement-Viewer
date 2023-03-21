b4b18b194acfb1a4fa64e1e875973e2481ba37eb
Ko1ors <kolorsnik181200@gmail.com>
2023-03-21 20:49:50 +0200

    Easiest Games page: fixed scrolling

---
+++ SteamAchievementViewer/Behaviors/IgnoreMouseWheelBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace SteamAchievementViewer.Behaviors
+{
+    public class IgnoreMouseWheelBehavior : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseWheel += AssociatedObjectPreviewMouseWheel;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObjectPreviewMouseWheel;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+
+            var mouseWheelEventArgs = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            {
+                RoutedEvent = UIElement.MouseWheelEvent
+            };
+            AssociatedObject.RaiseEvent(mouseWheelEventArgs);
+        }
+    }
+}
